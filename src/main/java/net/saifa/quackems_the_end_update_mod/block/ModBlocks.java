89c54d3256ae8126f2b4663829cd33f43f7a85e3
saifa <saifalbayatti@gmail.com>
2024-08-12 10:43:04 +0100

    Base

--- src/main/java/net/saifa/quackems_the_end_update_mod/block/ModBlocks.java
+++ src/main/java/net/saifa/quackems_the_end_update_mod/block/ModBlocks.java
@@ -1,9 +1,11 @@
 package net.saifa.quackems_the_end_update_mod.block;
 
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -21,10 +23,10 @@ public class ModBlocks {
             DeferredRegister.create(ForgeRegistries.BLOCKS, Quackems_The_End_Update.MOD_ID);
 
     public static final RegistryObject<Block> AMORITE_ORE = registerBlock("amorite_ore",
-            () -> new Block(BlockBehaviour.Properties.copy(Blocks.ANCIENT_DEBRIS).sound(SoundType.STONE)));
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.END_STONE).strength(6f).requiresCorrectToolForDrops(), UniformInt.of(9,15)));
 
     public static final RegistryObject<Block> AMORITE_BLOCK = registerBlock("amorite_block",
-            () -> new Block(BlockBehaviour.Properties.copy(Blocks.ANCIENT_DEBRIS).sound(SoundType.STONE)));
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE).strength(4f).requiresCorrectToolForDrops()));
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block){
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
